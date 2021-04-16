201d3686f5a61911fa2d91867bafe539c8f2f613
pratik Deo <pratikdeo78@gmail.com>
2021-04-16 14:17:25 -0400

    added note.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Note from './Note';
 
 import Footer from './Footer';
 function App(){
     return <div >
         <Header />
+        <Note />
         <Footer />
     </div>
 }
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Note(){
+    return (<div>
+        <h1>This is the title</h1>
+        <p>this is a paragraph</p>
+    </div>
+    );
+}
+
+
+
+export default Note;
