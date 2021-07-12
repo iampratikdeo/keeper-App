b90cbc714e591d9786bd5345d49295337f858d7a
Pratik Deo <pratikdeo@Pratiks-MacBook-Pro.local>
2021-07-12 16:42:49 -0400

    Updates

---
+++ precache-manifest.1dc03a921ac1b4afc6472c60760851a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3fc6b8582f5716b5fe6aabcbf91970",
+    "url": "/keeper-App/index.html"
+  },
+  {
+    "revision": "55c49cd08b825a731aca",
+    "url": "/keeper-App/static/js/2.3ee04d13.chunk.js"
+  },
+  {
+    "revision": "bdfff75fad2dc0fa532c",
+    "url": "/keeper-App/static/js/main.cb4017e5.chunk.js"
+  },
+  {
+    "revision": "f0e93aebffff4511189f",
+    "url": "/keeper-App/static/js/runtime-main.c5cfd94a.js"
+  }
+]);
